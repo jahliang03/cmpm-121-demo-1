361c1691305bb146253cb496696b46f3ddf5b6d2
jahliang03 <jahliang@ucsc.edu>
2024-10-16 00:40:04 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -15,19 +15,27 @@ const button = document.createElement("button");
 button.innerText = "🐻";
 app.append(button);
 
-let count: number = 0; 
+let count: number = 0;
 const countDiv = document.createElement("div");
 countDiv.innerText = `${count} bears`;
 
 app.append(countDiv);
 button.addEventListener("click", () => {
-    count += 1;
-    if(count != 1){
-        countDiv.innerText = `${count} bears`;
-    }
-    else{
-        countDiv.innerText = `${count} bear`;
-    }
+  incrementCounter();
 });
 
+// Make a new function called incrementCount
+const incrementCounter = () => {
+    count += 1; 
+    // If theres one bear set as that statement for proper grammar
+    if (count != 1) {
+        countDiv.innerText = `${count} bears`;
+    } else {
+        countDiv.innerText = `${count} bear`;
+    }
+}
+// Make it so that the counter counts up every second
+setInterval(incrementCounter, 1000);
+
+// Append elements to app container
 app.append(header, button, countDiv);
