a69e72b57b86e3cb13a1b416e7492f54a85315ec
jahliang03 <jahliang@ucsc.edu>
2024-10-16 00:18:45 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "jasmine liang";
+
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+//displayed in button
+button.innerText = "🐻";
+app.append(button);
+
+button.addEventListener('click', () => {
+    alert('Button was clicked!');
+});
